5048cf5921eee717958768c88782c0081ca38f7f
jayaratp <prasad.jayarathinam@outlook.com>
2016-08-11 23:19:55 +0100

    Product-Apple and Orange availability test - Success

--- src/main/scala/uk/gen/shop/stocks/FruitStockMaster.scala
+++ src/main/scala/uk/gen/shop/stocks/FruitStockMaster.scala
@@ -1,10 +1,12 @@
 package uk.gen.shop.stocks
 
+import uk.gen.shop.products.{Empty, Orange, Apple}
+
 /**
   * Created by PJAYARAT on 11/08/2016.
   */
 class FruitStockMaster extends StockMaster{
 
-  private val items=List("Apple")
-  override def getProduct(productName: String): String = items.find(_ == productName).getOrElse("")
+  private val items=List(Apple,Orange)
+  override def getProduct(productName: String): String = items.find(_.name == productName).getOrElse(Empty).name
 }
