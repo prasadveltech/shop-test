2a95341f1b9a7f13a1763773aa718fd13d08303f
jayaratp <prasad.jayarathinam@outlook.com>
2016-08-14 14:30:05 +0100

    Addition of promotional cart value total

--- src/main/scala/uk/gen/shop/till/Scanner.scala
+++ src/main/scala/uk/gen/shop/till/Scanner.scala
@@ -1,6 +1,7 @@
 package uk.gen.shop.till
 
 import uk.gen.shop.cart.SimpleShoppingCart
+import uk.gen.shop.offers.{FruitOffer, Offers}
 import uk.gen.shop.stocks.FruitStockMaster
 
 import scala.util.{Failure, Success, Try}
@@ -19,12 +20,15 @@ object Scanner {
         val cliScanner= new CommandLineScannerParser(fruitStockMaster)
         val parsedProducts = cliScanner.parseScannedString(scannedProducts)
         val cart = new SimpleShoppingCart
+        val cartWithOffer = new SimpleShoppingCart with FruitOffer
         parsedProducts.foreach(cart.addProduct(_))
+        parsedProducts.foreach(cartWithOffer.addProduct(_))
         println(s"scanned items are ${parsedProducts.map(_.name)} ")
         println(s"total simple cart value is ${cart.calculateTotal} ")
+        println(s"total offer cart value is ${cartWithOffer.calculateTotal} ")
       }
       case Failure(e) => {
-        println("Console Missing")
+        println("Scanner doesn't exist")
         System.exit(0)
       }
     }
