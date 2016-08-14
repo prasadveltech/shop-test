6abf48a441bc8176c46fc9ec14cd4845e967e339
jayaratp <prasad.jayarathinam@outlook.com>
2016-08-14 14:24:06 +0100

    Apple  and Orange Offer with Tests - Success

--- src/main/scala/uk/gen/shop/offers/FruitOffer.scala
+++ src/main/scala/uk/gen/shop/offers/FruitOffer.scala
@@ -1,7 +1,7 @@
 package uk.gen.shop.offers
 
 import uk.gen.shop.cart.ShoppingCart
-import uk.gen.shop.products.Apple
+import uk.gen.shop.products.{Orange, Apple}
 
 /**
   * Created by PJAYARAT on 14/08/2016.
@@ -9,17 +9,27 @@ import uk.gen.shop.products.Apple
 trait FruitOffer extends Offers{
 this: ShoppingCart =>
 
-  override  def chargeableCartProducts = applesToBeCharged
+  override  def chargeableCartProducts = applesToBeCharged ++ orangesToBeCharged
 
-  private def applesToBeCharged = cartProducts.take(countAppleToBeCharged(cartProducts.count(_.name == Apple.name)))
+  private def applesToBeCharged = cartProducts.filter(_.name == Apple.name).take(countApplesToBeCharged(cartProducts.count(_.name == Apple.name)))
 
-  private def countAppleToBeCharged(appleCount: Int) = {
+  private def orangesToBeCharged = cartProducts.filter(_.name == Orange.name).take(countOrangesToBeCharged(cartProducts.count(_.name == Orange.name)))
 
+  private def countApplesToBeCharged(appleCount: Int) = {
     appleCount match {
       case 0 => 0
       case 1 => 1
-      case cnt if cnt%2 == 0 => cnt - cnt/2
-      case cnt if cnt%2 == 1 => cnt - cnt/2
+      case cnt => cnt - cnt/2
+    }
+  }
+
+
+  private def countOrangesToBeCharged(orangeCount: Int) = {
+    orangeCount match {
+      case 0 => 0
+      case 1 => 1
+      case 2 => 2
+      case cnt => cnt - cnt/3
     }
 
   }
--- src/test/scala/uk/gen/shop/cart/ShoppingCartWithOffersSpec.scala
+++ src/test/scala/uk/gen/shop/cart/ShoppingCartWithOffersSpec.scala
@@ -2,7 +2,7 @@ package uk.gen.shop.cart
 
 import org.scalatest.{Matchers, GivenWhenThen, FeatureSpec}
 import uk.gen.shop.offers.FruitOffer
-import uk.gen.shop.products.{Empty, Apple}
+import uk.gen.shop.products.{Orange, Empty, Apple}
 
 /**
   * Created by PJAYARAT on 14/08/2016.
@@ -73,6 +73,41 @@ class ShoppingCartWithOffersSpec extends FeatureSpec with GivenWhenThen with Mat
       offerCart.chargeableCartProducts.size should be(2)
       offerCart.calculateTotal should be(1.2)
     }
+
+
+    scenario("Test cart of 3 oranges price with offers") {
+      Given("a shopping cart along with offers")
+      val offerCart = new SimpleShoppingCart with FruitOffer
+
+      When("adding 3 oranges")
+      offerCart.addProduct(Orange)
+      offerCart.addProduct(Orange)
+      offerCart.addProduct(Orange)
+
+      Then("two oranges should be charged")
+      offerCart.chargeableCartProducts.size should be(2)
+      offerCart.calculateTotal should be(0.5)
+    }
+
+
+    scenario("Test cart of 2 apples and 3 oranges price with offers") {
+      Given("a shopping cart along with offers")
+      val offerCart = new SimpleShoppingCart with FruitOffer
+
+      And("adding 3 oranges")
+      offerCart.addProduct(Orange)
+      offerCart.addProduct(Orange)
+      offerCart.addProduct(Orange)
+
+      And("adding 2 apples")
+      offerCart.addProduct(Apple)
+      offerCart.addProduct(Apple)
+
+      Then("two oranges and 1 apple should be charged")
+      offerCart.chargeableCartProducts.size should be(3)
+      offerCart.calculateTotal should be(1.1)
+    }
+
   }
 
 }
